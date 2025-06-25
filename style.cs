78a9d948d82170287d5ae28a133cb78c4a7bad70
mcrbilly7 <nossonkosowsky@gmail.com>
2025-06-24 22:09:25 -0500

    Update and rename script.js to style.cs

---
+++ style.cs
@@ -0,0 +1,111 @@
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  font-family: 'Courier New', monospace;
+  background: #f0f0f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.game-container {
+  width: 100%;
+  max-width: 800px;
+  height: 450px;
+  background: #fff;
+  border: 4px solid #222;
+  position: relative;
+  overflow: hidden;
+}
+.game {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#cce, #eef);
+}
+.dino {
+  width: 50px;
+  height: 50px;
+  background: url('https://i.imgur.com/OdL0XPt.png') no-repeat center/cover;
+  position: absolute;
+  bottom: 60px;
+  left: 50px;
+}
+.obstacle {
+  position: absolute;
+}
+.cactus-small {
+  width: 20px;
+  height: 40px;
+  background: url('https://i.imgur.com/MlqLh1r.png') no-repeat center/cover;
+  bottom: 60px;
+}
+.cactus-large {
+  width: 30px;
+  height: 60px;
+  background: url('https://i.imgur.com/eaVwVp3.png') no-repeat center/cover;
+  bottom: 60px;
+}
+.bird {
+  width: 50px;
+  height: 35px;
+  background: url('https://i.imgur.com/SfH5QMa.png') no-repeat center/cover;
+  bottom: 130px;
+}
+.ground {
+  position: absolute;
+  width: 100%;
+  height: 20px;
+  background: #444;
+  bottom: 40px;
+}
+.score, .high-score {
+  position: absolute;
+  top: 10px;
+  font-size: 18px;
+  color: #222;
+}
+.score { left: 10px; }
+.high-score { right: 10px; }
+.game-over {
+  position: absolute;
+  top: 40%;
+  width: 100%;
+  text-align: center;
+  display: none;
+}
+.start-screen {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: #fff;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+button {
+  font-size: 18px;
+  padding: 10px 20px;
+  margin-top: 10px;
+  cursor: pointer;
+  border: 2px solid #222;
+  background: #eee;
+}
+
+.power-up {
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  z-index: 2;
+}
+.shield {
+  background: radial-gradient(cyan, blue);
+}
+.boost {
+  background: radial-gradient(orange, red);
+}
